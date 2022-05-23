8bb95d2540ffff29604bbb43dbedb0d4f7fda3bd
Scarlettlp <scarlett.lara.p@gmail.com>
2022-05-22 21:05:40 -0500

    href updates

--- src/Weather.js
+++ src/Weather.js
@@ -3,39 +3,38 @@ import "./Weather.css";
 
 export default function Weather() {
   return (
-    <div class="container">
-      <div class="weather-app-wrapper">
-        <div class="weather-app">
+    <div className="container">
+      <div className="weather-app-wrapper">
+        <div className="weather-app">
           <form>
             <input type="search" placeholder="Enter a city..." />
             <input type="submit" value="Search" />
             <input type="submit" value="Current" />
           </form>
 
-          <h1 class="date"> Sunday 07:43</h1>
-          <div class="row">
-            <div class="col-6">
-              <div class="clearfix">
+          <h1 className="date"> Sunday 07:43</h1>
+          <div className="row">
+            <div className="col-6">
+              <div className="clearfix">
                 <img
                   src="http://openweathermap.org/img/wn/10d@2x.png"
-                  id="icon"
-                  class="float-left"
+                  className="float-left"
                 />
 
                 <div class="temperature">
                   <span id="temp">22</span>{" "}
-                  <a href="#" id="celsius-link" class="active">
+                  <a href="/" id="celsius-link" class="active">
                     {" "}
                     °C{" "}
                   </a>
                   |
-                  <a href="#" id="fahrenheit-link">
+                  <a href="/">
                     °F
                   </a>
                 </div>
               </div>
             </div>
-            <div class="col-6">
+            <div className="col-6">
               <ul>
                 <li>
                   Humidity: <span id="humidity"> 30</span>%
@@ -46,19 +45,19 @@ export default function Weather() {
               </ul>
             </div>
           </div>
-          <div class="row">
-            <div class="col-2">
-              <span class="sky" id="description">
+          <div className="row">
+            <div className="col-2">
+              <span className="sky" id="description">
                 Clear
               </span>
             </div>
             <h2 id="city">San Francisco, California, EU.</h2>
           </div>
           <br />
-          <div class="weather-forecast" id="forecast"></div>
+          <div className="weather-forecast" id="forecast"></div>
         </div>
         <small>
-          <a href="https://github.com/Scarlettlp/Weather-app" target="_blank">
+          <a href="https://github.com/Scarlettlp/Weather-app">
             Open-source code
           </a>
           by Scarlett Lara
